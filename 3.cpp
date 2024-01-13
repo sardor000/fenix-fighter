acd3d5e6886591749c95a8b908e0da877775fb06
sardor000 <156397410+sardor000@users.noreply.github.com>
2024-01-13 10:03:29 +0500

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+ int main()
+company the booking{
+    Title title;
+    String name;
+    Location location;
+    int price;
+    int volume ;
+    Way of Paymet way of paymat;
+    State state ;
+
+    Booking booking (){
+        title = Title.Tima Stek;
+        name = "";
+        price = 0;
+        volume = 0;
+    }
+    void booking(State state){
+        this.state = state;
+    }
+    void moveTo(Point point){
+        if state == State.hotel{
+            volume = 100;
+            price = 50;
+            
+        }
+    }
+    return 0;
+}
